3d36023bfe5cfa8a8f1536fc3919cc63917ecc27
Radoslaw Wojaczek <radoslaw_wojaczek@epam.com>
2022-10-14 11:52:16 +0200

    create two components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Headline from './Greeting/Greeting';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React.createElement</h1>
+      <br></br>
+      <h1>React.Component</h1>
+      <br></br>
+      <h1>React.PureComponent</h1>
+      <br></br>
+      <h1>Functional component</h1>
+     <Headline/>
+    </div>
+  );
+}
+
+export default App;
